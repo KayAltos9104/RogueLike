1f36f7767a9e36e1ff2225fde44093dd842a9554
Kiselev Anton <anrohakiselev117@gmail.com>
2022-11-15 21:08:54 +0300

    Feat : добавил генерацию поля и отображение оного

--- RogueLike/Program.cs
+++ RogueLike/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-
+using ROGUEeng;
 namespace RogueLike
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            int a = 25;
+            int b = 25;
+            Space sp = new Space(a, b);
+            sp.RenderSpace();
         }
     }
 }
